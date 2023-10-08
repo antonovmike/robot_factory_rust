43a9eef3688c66f399d7d91c5622d592d07c2369
antonovmike <antonovma@outlook.com>
2023-10-08 09:26:41 +0400

    refactor report.rs

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,4 @@ pub const DATABASE_NAME: &str = "db.sqlite3";
 pub const CHECK_INTERVAL: u64 = 4;
 pub const SMTP_SERVER: &str = "example.com";
 pub const SMTP_SENDER: &'static str = "noreply@example.com";
+pub const SHEET_HEADERS: [&str; 3] = ["Model", "Version", "Quantity per week"];
--- src/report.rs
+++ src/report.rs
@@ -9,39 +9,43 @@ use axum::{
 };
 use http::header::CONTENT_TYPE;
 use rusqlite::Result;
-use rust_xlsxwriter::Workbook;
+use rust_xlsxwriter::{Workbook, Worksheet, XlsxError};
 use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 
 use crate::constants::*;
 
-pub fn create_xlsx() -> Result<(), anyhow::Error> {
+fn create_xlsx() -> Result<(), anyhow::Error> {
     // Check if the file exists and delete it if it does
     if fs::metadata(PATH_TO_XLSX).is_ok() {
-        fs::remove_file(PATH_TO_XLSX).unwrap();
+        fs::remove_file(PATH_TO_XLSX)?;
     }
 
-    let conn = rusqlite::Connection::open(DATABASE_NAME).unwrap();
-    let mut stmt = conn
-        .prepare(
-            "SELECT model, version, COUNT(*) as count FROM robots 
-            WHERE created >= date('now', '-7 day') GROUP BY model, version",
-        )
-        .unwrap();
-    let robots_iter = stmt
-        .query_map([], |row| {
-            Ok((
-                row.get::<_, String>(0)?, // model
-                row.get::<_, String>(1)?, // version
-                row.get::<_, i64>(2)?,    // count
-            ))
-        })
-        .unwrap();
-    let robots: Result<Vec<_>, _> = robots_iter.collect();
-    let robots = robots.unwrap();
+    let conn = rusqlite::Connection::open(DATABASE_NAME)?;
+    let robots = fetch_robots(&conn)?;
 
-    // Create a HashMap where the key is the first letter of the model
-    // and the value is a vector of tuples (model, version, count)
+    let groups = group_robots_by_model(robots);
+    create_excel_file(groups).unwrap();
+
+    Ok(())
+}
+
+fn fetch_robots(conn: &rusqlite::Connection) -> rusqlite::Result<Vec<(String, String, i64)>> {
+    let mut stmt = conn.prepare(
+        "SELECT model, version, COUNT(*) as count FROM robots 
+        WHERE created >= date('now', '-7 day') GROUP BY model, version",
+    )?;
+    let robots_iter = stmt.query_map([], |row| {
+        Ok((
+            row.get::<_, String>(0)?, // model
+            row.get::<_, String>(1)?, // version
+            row.get::<_, i64>(2)?,    // count
+        ))
+    })?;
+    robots_iter.collect()
+}
+
+fn group_robots_by_model(robots: Vec<(String, String, i64)>) -> HashMap<char, Vec<(String, String, i64)>> {
     let mut groups: HashMap<char, Vec<(String, String, i64)>> = HashMap::new();
     for (model, version, count) in robots {
         let first_char = model.chars().next().unwrap();
@@ -50,33 +54,39 @@ pub fn create_xlsx() -> Result<(), anyhow::Error> {
             .or_insert_with(Vec::new)
             .push((model, version, count));
     }
+    groups
+}
 
-    // Create a new Excel file
+fn create_excel_file(groups: HashMap<char, Vec<(String, String, i64)>>) -> Result<(), Box<dyn std::error::Error>> {
     let mut workbook = Workbook::new();
 
-    // Iterate over the groups and create a new sheet for each group
     for (key, value) in &groups {
         let sheet_name = format!("{}", key);
-        let sheet = workbook.add_worksheet().set_name(sheet_name).unwrap();
-        sheet.write_string(0, 0, "Model").unwrap();
-        sheet.write_string(0, 1, "Version").unwrap();
-        sheet.write_string(0, 2, "Quantity per week").unwrap();
-
-        // Write the data for each group to the sheet
-        for (i, (model, version, count)) in value.iter().enumerate() {
-            sheet
-                .write_string(i as u32 + 1, 0, model.to_string())
-                .unwrap();
-            sheet
-                .write_string(i as u32 + 1, 1, version.to_string())
-                .unwrap();
-            sheet.write_number(i as u32 + 1, 2, *count as f64).unwrap();
-        }
+        let sheet = workbook.add_worksheet().set_name(sheet_name)?;
+        write_headers(sheet)?;
+        write_data(sheet, value)?;
     }
 
     workbook.save(PATH_TO_XLSX).unwrap();
     Ok(())
 }
+
+fn write_headers(sheet: &mut Worksheet) -> Result<(), XlsxError> {
+    for (i, header) in SHEET_HEADERS.iter().enumerate() {
+        sheet.write_string(0, i as u16, header.to_string()).unwrap();
+    }
+    Ok(())
+}
+
+fn write_data(sheet: &mut Worksheet, data: &[(String, String, i64)]) -> Result<(), Box<dyn std::error::Error>> {
+    for (i, (model, version, count)) in data.iter().enumerate() {
+        sheet.write_string((i + 1) as u32, 0, model)?;
+        sheet.write_string((i + 1) as u32, 1, version)?;
+        sheet.write_number((i + 1) as u32, 2, *count as f64)?;
+    }
+    Ok(())
+}
+
 
 pub async fn report_handler() -> Result<impl IntoResponse, (StatusCode, String)> {
     match tokio::task::spawn(async { create_xlsx() }).await {
