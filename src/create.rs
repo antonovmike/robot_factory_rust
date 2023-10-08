a564e41f26d91654aa9bde411029d8b258e5d2dd
antonovmike <antonovma@outlook.com>
2023-10-08 08:53:10 +0400

    refactor create.rs

--- src/create.rs
+++ src/create.rs
@@ -5,19 +5,31 @@ use rusqlite::{Connection, Result};
 use validator::Validate;
 
 use crate::constants::DATABASE_NAME;
-use crate::structures::Robot;
 use crate::db::setup_database;
+use crate::structures::Robot;
+
+// Проверяем данные на валидность
+fn validate_robot(robot: &Robot) -> Result<(), StatusCode> {
+    if robot.validate().is_err() {
+        Err(StatusCode::BAD_REQUEST)
+    } else {
+        Ok(())
+    }
+}
+
+// Открываем соединение с базой данных
+fn open_database() -> Result<Connection, StatusCode> {
+    match Connection::open(Path::new(DATABASE_NAME)) {
+        Ok(conn) => Ok(conn),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
 
 pub async fn create_robot(Json(robot): Json<Robot>) -> Result<StatusCode, StatusCode> {
-    // Проверяем данные на валидность
-    if robot.validate().is_err() {
-        return Err(StatusCode::BAD_REQUEST);
-    }
-    // Открываем соединение с базой данных
-    let conn = match Connection::open(Path::new(DATABASE_NAME)) {
-        Ok(conn) => conn,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
-    };
+    validate_robot(&robot)?;
+
+    let conn = open_database()?;
+
     // Создаем таблицу robots, если ее не существует
     match setup_database() {
         Ok(_) => (),
@@ -36,18 +48,15 @@ pub async fn create_robot(Json(robot): Json<Robot>) -> Result<StatusCode, Status
 }
 
 pub async fn remove_robot(Json(robot): Json<Robot>) -> Result<StatusCode, StatusCode> {
-    // Проверяем данные на валидность
-    if robot.validate().is_err() {
-        return Err(StatusCode::BAD_REQUEST);
-    }
-    let conn = match Connection::open(Path::new(DATABASE_NAME)) {
-        Ok(conn) => conn,
-        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
-    };
+    validate_robot(&robot)?;
+
+    let conn = open_database()?;
+
     let statement = format!(
         "DELETE FROM robots WHERE serial = '{}' AND model = '{}' AND version = '{}'",
         &robot.serial, &robot.model, &robot.version
     );
+    
     match conn.execute(&statement, []) {
         Ok(rows_affected) => {
             if rows_affected > 0 {
@@ -57,11 +66,10 @@ pub async fn remove_robot(Json(robot): Json<Robot>) -> Result<StatusCode, Status
                 println!("Robot not found");
                 Err(StatusCode::NOT_FOUND)
             }
-        },
+        }
         Err(_) => {
             println!("An error occurred while attempting to remove the robot");
             Err(StatusCode::INTERNAL_SERVER_ERROR)
-        },
+        }
     }
 }
-
