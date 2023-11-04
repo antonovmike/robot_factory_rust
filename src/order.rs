d0ca76ac9a3bf733acffde1b96df6b6044cf4cbd
antonovmike <antonovma@outlook.com>
2023-11-04 11:01:52 +0400

    refactor orders.rs

--- src/order.rs
+++ src/order.rs
@@ -6,17 +6,17 @@ use crate::db::open_database;
 // use crate::structures::Order;
 
 pub async fn add_order(customer_name: String, robot_model: String) -> Result<StatusCode, StatusCode> {
-    let pool = open_database().await?;
+    let pool = open_database().await.expect("Failed to open database");
 
     let order_date = Utc::now().to_rfc3339();
     
     let statement = format!(
-        "INSERT INTO orders (customer_name, robot_model, order_date) VALUES ($1, $2, '{order_date}')"
+        r#"INSERT INTO orders (customer_name, robot_model, order_date) VALUES (\$1, \$2, '{order_date}')"#
     );
 
     match sqlx::query(&statement)
-        .bind(customer_name)
-        .bind(robot_model)
+        .bind(&customer_name)
+        .bind(&robot_model)
         .execute(&pool)
         .await
     {
